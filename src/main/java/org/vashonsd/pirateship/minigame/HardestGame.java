4ddcf3187d63e858db8c6958f71c0fd0d42bec06
weller.ryan <weller.ryan@VL-CTE-07.vanguard.vashonsd.org>
2016-03-24 09:16:14 -0700

    added hardest game

--- src/main/java/org/vashonsd/pirateship/minigame/HardestGame.java
+++ src/main/java/org/vashonsd/pirateship/minigame/HardestGame.java
@@ -25,8 +25,8 @@ public class HardestGame implements Minigame
 
 	public String Handle(String s)
 	{
-		String[] life = {"You made money.", "Someone asked you for money and you made them dance for it... *ss hole.", "You donated to charity.", "Your stocks went up.", "You pooped today!", "Apple pie for breakfast."};
-		String[] death = {"You were suffocated by a mob of Apple fanatics.", "Your intern put cinnamon in you coffee and you're allergic to it", "A tiger bit your head off at a party.", "You went on a vacation to Africa but were captured by tribesmen. The last thing you heard was 'whaki buhtaki maki laki'", "Microsoft decided to 'take you out'.", "Drugs are bad"};
+		String[] life = {"You made money.", "You preformed in an oprah.", "You donated to charity.", "Your stocks went up.", "You pooped today!", "Apple pie for breakfast.", "You learned how telepathy.", "You got a pet bear.", "You met Jim Carrey, and shared a burger with him.", "I KNEW YOU WERE TROUBLE WHEN YOU WALKED IN SO SHAME ON ME NOW!", "Wiki wiki liki shmicky ticky.", "You had a taco in mexico.", "You had rice in China with your native friend: Ching Ho Ma Chow Mein Wa Fu.", "You wasted your life away on an RPG made by some high school kid."};
+		String[] death = {"You were suffocated by a mob of Apple fanatics.", "Your intern put cinnamon in you coffee and you're allergic to it.", "A tiger bit your head off at a party.", "You went on a vacation to Africa but were captured by tribesmen. The last thing you heard was 'whaki buhtaki maki laki'", "Microsoft decided to 'take you out'.", "Drugs are bad.", "You walked under a ladder.", "One of the heads on Easter Island fell on you."};
 		Random rnd = new Random();
 		int theNumber = rnd.nextInt(life.length);
 		int theNumber2 = rnd.nextInt(death.length);
@@ -39,8 +39,12 @@ public class HardestGame implements Minigame
 		}
 		else
 		{
+			int days = day - 1;
+			int monies = money;
 			day = 0;
-			return death[theNumber2] + " You died.\n\nYou survived " + day + " days, and made " + money + " million dollars.\n\nHit enter to play again?";
+			money = 0;
+			return death[theNumber2] + " You died.\n\nYou survived " + days + " days, and made " + monies + " million dollars.\n\nHit enter to play again?";
+			
 		}
 	}
 
